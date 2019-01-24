371abe3ccd39e8dde7cb0ca83db6463195bdb3b5
Thomas Svartbäck <thomsva@gmail.com>
2019-01-24 15:37:51 +0200

    1.6 valmis

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const increaseGoodByOne = () => setGood(good + 1)
+  const increaseNeutralByOne = () => setNeutral(neutral + 1)
+  const increaseBadByOne = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>anna palautetta</h1>
+      <button onClick={increaseGoodByOne}>hyvä</button>
+      <button onClick={increaseNeutralByOne}>neutraali</button>
+      <button onClick={increaseBadByOne}>huono</button>
+      <h1>statistiikka</h1>
+      <p>hyvä {good}</p>
+      <p>neutraali {neutral}</p>
+      <p>huono {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
