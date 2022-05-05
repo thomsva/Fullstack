b6895df572cd07a4e23d2499ba9d27a5ae126239
thomsva <thomsva@gmail.com>
2022-05-05 11:08:09 +0300

    12.7 ok maybe

--- Part12/todo-app/todo-backend/routes/todos.js
+++ Part12/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,13 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  await Todo.findOne({_id: req.todo._id}, req.todo)
+  res.send(req.todo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
