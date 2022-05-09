d1ecc96f710a098cc1fb9a783c2d288ef6254b64
thomsva <thomsva@gmail.com>
2022-05-09 15:35:40 +0300

    10.7 done

--- Part10/rate-repository-app/src/components/AppBar.jsx
+++ Part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,46 +1,53 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView} from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import Text from './Text';
 import { Link } from 'react-router-native';
 
-
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: theme.palette.primary,
     flexDirection: 'row',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: theme.palette.primary,
-    flexWrap: 'nowrap',
-    height: 80,
-    alignItems: 'center',
     padding: 8,
-    
+    flexWrap: 'nowrap',
+    display: 'flex',
+    height: 70,
+    alignItems: 'flex-end'
+  },
+  scrollHorisontal: {
+    flexDirection: 'row',
+  },
+  tabsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
   },
   itemSmall: {
-    flexGrow: 0,
-    justifyContent: 'center',
-    padding: 8,
+    flexGrow: 1,
+    padding: 8
   },
   itemLarge: {
     flexGrow: 1,
-    justifyContent: 'center',
-    padding: 8,
-  }
+    padding: 8
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to='/' style={styles.itemLarge}>
-        <Text fontSize='tabheading' color='light'>Repositories</Text>
-      </Link>
-      <Link to='/signIn' style={styles.itemLarge}>
-        <Text fontSize='tabheading' color='light'>Sign in</Text>
-      </Link>
+    <ScrollView horisontal style={styles.scrollHorisontal}>
+      <View style={styles.tabsContainer}>
+        <Link to='/' style={styles.itemLarge} text='Repositories' component={View}>
+          <Text fontSize='tabheading' fontWeight='bold' color='light'>Repositories</Text>
+        </Link>
+        <Link to='/signIn' style={styles.itemLarge} component={View}>
+          <Text fontSize='tabheading' fontWeight='bold' color='light'>Sign in</Text>
+        </Link>
+      </View>
+    </ScrollView>
     </View>
+  
   );
 };
 
 export default AppBar;
-
-
