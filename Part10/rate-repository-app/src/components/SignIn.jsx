5e7963934ebf1b5d370b5f7ee955ee09c17cc610
thomsva <thomsva@gmail.com>
2022-05-09 17:03:33 +0300

    10.8 basic functionality implemented

--- Part10/rate-repository-app/src/components/SignIn.jsx
+++ Part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,40 @@
 import Text from './Text';
+import { Pressable, View } from 'react-native';
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput';
+
+const initialValues = {
+  username: 'x',
+  password: 'x',
+};
+
+const SignInForm = ({ onSubmit }) => {
+
+  return (
+    <View>
+      <FormikTextInput
+        name='username'
+        placeholder='User name'
+      />
+      <FormikTextInput
+        name='password'
+        placeholder='Password'
+      />
+      <Pressable onPress={onSubmit}>
+        <Text>Submit</Text>
+      </Pressable>
+    </View>
+  )
+}
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = values => console.log(values);
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
