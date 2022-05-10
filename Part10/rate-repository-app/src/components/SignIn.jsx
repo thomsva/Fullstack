d75b22cb1fe89aed3abde4a9eab9f49c4dcae9ae
thomsva <thomsva@gmail.com>
2022-05-10 16:53:38 +0300

    10.13 ok:ish

--- Part10/rate-repository-app/src/components/SignIn.jsx
+++ Part10/rate-repository-app/src/components/SignIn.jsx
@@ -1,6 +1,7 @@
 import { Formik } from 'formik';
 import SignInForm from './SignInForm';
 import * as yup from 'yup';
+import useSignIn from '../hooks/useSignIn';
 
 const initialValues = {
   username: '',
@@ -17,7 +18,19 @@ const validationSchema = yup.object().shape({
 });
 
 const SignIn = () => {
-  const onSubmit = values => console.log(values);
+  const [signIn, result] = useSignIn();
+
+  const onSubmit = async values => {
+    const { username, password } = values;
+    try {
+      await signIn({ username, password })
+    } catch (e) {
+      console.error(e)
+    }
+  };
+
+  // Result will contain token if credentials are ok
+  if (!result.loading) console.log('result:', result)
 
   return (
     <Formik
---
+++ Part10/rate-repository-app/src/graphql/mutations.js
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const SIGN_IN = gql`
+  mutation authenticate($credentials: AuthenticateInput!) {
+    authenticate(credentials: $credentials) {
+      accessToken
+    }
+  }
+`; 
+
+
