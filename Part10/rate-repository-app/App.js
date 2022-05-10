30aa8aa8cb6f1e0af439addc9228d06e17adc522
thomsva <thomsva@gmail.com>
2022-05-10 14:47:12 +0300

    10.12

--- Part10/rate-repository-app/App.js
+++ Part10/rate-repository-app/App.js
@@ -2,9 +2,11 @@ import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
 import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
+console.log(Constants.manifest.extra);
 const App = () => {
   return (
     <NativeRouter>
